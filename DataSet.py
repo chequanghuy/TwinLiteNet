804d6c86df96e4b1f98bf7a420975259f14d6671
kimik76 <147421019+kimik76@users.noreply.github.com>
2024-03-09 15:20:54 +0330

    Update DataSet.py

--- DataSet.py
+++ DataSet.py
@@ -242,10 +242,10 @@ class IADDataset(torch.utils.data.Dataset):
         self.Tensor = transforms.ToTensor()
         self.valid=valid
         if valid:
-            self.root='/kaggle/working/IADD/train/img'
+            self.root='/kaggle/working/IADD/IADDv5/train/img'
             self.names=os.listdir(self.root)
         else:
-            self.root='/kaggle/working/IADD/train/img'
+            self.root='/kaggle/working/IADD/IADDv5/train/img'
             self.names=os.listdir(self.root)#[:1000]
 
     def __len__(self):
