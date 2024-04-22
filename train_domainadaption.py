7ad3825b02bc291284a3180cadbbbd8dcf3b9210
kimik76 <147421019+kimik76@users.noreply.github.com>
2024-04-22 15:07:27 +0330

    Update train_domainadaption.py

--- train_domainadaption.py
+++ train_domainadaption.py
@@ -16,10 +16,10 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 import torch.nn.functional as F
-if not os.path.isfile('/kaggle/working/iadd/ll'):
+if not os.path.isdir('/kaggle/working/iadd/ll'):
     os.mkdir('/kaggle/working/iadd/ll')
     print('making ll folder')
-if not os.path.isfile('/kaggle/working/iadd/da'):
+if not os.path.isdir('/kaggle/working/iadd/da'):
     os.mkdir('/kaggle/working/iadd/da')
     print('making da folder')
 
