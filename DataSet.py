8fafe3e83ebd91786a8042f8e4d7393659423a40
kimik76 <147421019+kimik76@users.noreply.github.com>
2024-04-27 15:53:51 +0330

    Update DataSet.py

--- DataSet.py
+++ DataSet.py
@@ -536,3 +536,84 @@ class first_pseudo_label_dataset(torch.utils.data.Dataset):
           
         return image_name,image
 
+class BDDataset(torch.utils.data.Dataset):
+    '''
+    Class to load the dataset
+    '''
+    def __init__(self, transform=None,valid=False):
+        '''
+        :param imList: image list (Note that these lists have been processed and pickled using the loadData.py)
+        :param labelList: label list (Note that these lists have been processed and pickled using the loadData.py)
+        :param transform: Type of transformation. SEe Transforms.py for supported transformations
+        '''
+
+        self.transform = transform
+        self.Tensor = transforms.ToTensor()
+        self.valid=valid
+        if valid:
+            self.root='/kaggle/input/bdd100k-dataset/bdd100k/bdd100k/images/100k/val'
+            self.names=os.listdir(self.root)
+        else:
+            self.root='/kaggle/input/bdd100k-dataset/bdd100k/bdd100k/images/100k/train'
+            self.names=os.listdir(self.root)[:1000]
+
+    def __len__(self):
+        return len(self.names)
+
+    def __getitem__(self, idx):
+        '''
+
+        :param idx: Index of the image file
+        :return: returns the image and corresponding label file.
+        '''
+        W_=512
+        H_=512
+        image_name=os.path.join(self.root,self.names[idx])
+        
+        image = cv2.imread(image_name)
+        if self.valid:
+            label1 = cv2.imread(image_name.replace("input/bdd100k-dataset/bdd100k/bdd100k/images/100k","working/labels/bdd_seg_gt").replace("jpg","png"), 0)
+            label2 = cv2.imread(image_name.replace("input/bdd100k-dataset/bdd100k/bdd100k/images/100k","working/labels/bdd_lane_gt").replace("jpg","png"), 0) 
+        else:
+            label1 = cv2.imread(image_name.replace("input/bdd100k-dataset/bdd100k/bdd100k/images/100k/train","working/iadd/da").replace(".jpg",".png"), 0)
+            label2 = cv2.imread(image_name.replace("input/bdd100k-dataset/bdd100k/bdd100k/images/100k/train","working/iadd/da").replace(".jpg",".png"), 0)   
+        if not self.valid:
+            if random.random()<0.5:
+                combination = (image, label1, label2)
+                (image, label1, label2)= random_perspective(
+                    combination=combination,
+                    degrees=10,
+                    translate=0.1,
+                    scale=0.25,
+                    shear=0.0
+                )
+            if random.random()<0.5:
+                augment_hsv(image)
+            if random.random() < 0.5:
+                image = np.fliplr(image)
+                label1 = np.fliplr(label1)
+                label2 = np.fliplr(label2)
+            
+        label1 = cv2.resize(label1, (W_, H_))
+        label2 = cv2.resize(label2, (W_, H_))
+        image = cv2.resize(image, (W_, H_))
+
+        _,seg_b1 = cv2.threshold(label1,0,255,cv2.THRESH_BINARY_INV)
+        _,seg_b2 = cv2.threshold(label2,0,255,cv2.THRESH_BINARY_INV)
+        _,seg1 = cv2.threshold(label1,0,255,cv2.THRESH_BINARY)
+        _,seg2 = cv2.threshold(label2,0,255,cv2.THRESH_BINARY)
+
+        seg1 = self.Tensor(seg1)
+        seg2 = self.Tensor(seg2)
+        seg_b1 = self.Tensor(seg_b1)
+        seg_b2 = self.Tensor(seg_b2)
+        seg_da = torch.stack((seg_b1[0], seg1[0]),0)
+        seg_ll = torch.stack((seg_b2[0], seg2[0]),0)
+        # image = image[:, :, ::-1].transpose(2, 0, 1)
+        image = np.ascontiguousarray(image)
+
+        if self.transform is not None :
+            image = self.transform(image)
+          
+        return image_name,image,(seg_da,seg_ll)
+
