e3a8852b12e98744aec107aaa86d1ffd412c34c2
kimia <kimkam1376@gmail.com>
2024-04-29 19:47:55 +0330

    chert_update

--- train_domainadaption.py
+++ train_domainadaption.py
@@ -36,14 +36,14 @@ def train_net(args):
     pretrained = args.pretrained
     if pretrained is not None:
         model = create_seg_model('b0', 'bdd', weight_url=pretrained)
-        if args.pseudo:
-            pseudo_data = torch.utils.data.DataLoader(
-                first_pseudo_label_dataset2(transform=transform, valid=False),
-                batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
-            print(' pseudo label makering using the pretrained weights')
-            pseudo_label_maker(pseudo_data, model)
-        else:
-            print('pseudo labels are already made')
+        # if args.pseudo:
+        #     pseudo_data = torch.utils.data.DataLoader(
+        #         first_pseudo_label_dataset2(transform=transform, valid=False),
+        #         batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+        #     print(' pseudo label makering using the pretrained weights')
+        # #     pseudo_label_maker(pseudo_data, model)
+        # else:
+        #     print('pseudo labels are already made')
     else:
         model = create_seg_model('b0', 'bdd', False)
 
