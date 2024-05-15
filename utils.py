abb582b224465bb9cc997dc5558fa1c0016cc36c
kimia <kimkam1376@gmail.com>
2024-05-15 11:28:50 +0330

    change pbar

--- utils.py
+++ utils.py
@@ -124,14 +124,14 @@ def train(args, source_loader, target_loader, model, criterion, criterion_mmd, o
 
     total_batches = len(source_loader)
     target_loader = cycle(iter(target_loader))
-    source_loader = iter(source_loader)
+    source_loader = (source_loader)
     # pbar = enumerate(zip(source_loader, cycle(target_loader)))
     LOGGER.info(('\n' + '%13s' * 5) % ('Epoch', 'TverskyLoss', 'FocalLoss', 'MMDLoss', 'TotalLoss'))
     # pbar = tqdm(pbar, total=total_batches, )
-    pbar = tqdm(range(total_batches), bar_format='{l_bar}{bar:10}{r_bar}')
-    for i in pbar:
-        (_, source_input, source_label) = source_loader.__new__()
-        (_, target_input, _) = target_loader.__new__()
+    pbar = tqdm(source_loader, total=total_batches, bar_format='{l_bar}{bar:10}{r_bar}')
+    for i, (source_data) in pbar:
+        (_, source_input, source_label) = source_data
+        (_, target_input, _) = target_loader.__next__()
         if args.device == 'cuda:0':
             source_input = source_input.cuda().float()
             source_label[0] = source_label[0].cuda()
