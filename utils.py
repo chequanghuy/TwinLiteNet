ad3eb9547889c81f3af7be14b0aae7d230868c30
kimia <kimkam1376@gmail.com>
2024-05-23 12:06:08 +0330

    change pbar

--- utils.py
+++ utils.py
@@ -254,7 +254,7 @@ def val(val_loader, model):
         target_var = target
 
         with torch.no_grad():
-            _, output = model(source_input, None, 'source')
+            _, output = model(input_var, None, 'source')
             # output = model(input_var)
             output = (resize(output[0], [512, 512]), resize(output[1], [512, 512]))
 
