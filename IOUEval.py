7762c08c0566b3673f358067a857ee85aadb6b89
Chế Quang Huy <49285058+chequanghuy@users.noreply.github.com>
2025-03-10 12:46:49 +0700

    Update IOUEval.py

--- IOUEval.py
+++ IOUEval.py
@@ -64,7 +64,9 @@ class SegmentationMetric(object):
         # acc = (TP + TN) / (TP + TN + FP + TN)
         acc = np.diag(self.confusionMatrix).sum() /  self.confusionMatrix.sum()
         return acc
-        
+    def lineAccuracy(self):
+        Acc = np.diag(self.confusionMatrix) / (self.confusionMatrix.sum(axis=1) + 1e-12)
+        return Acc[1]
 
     def classPixelAccuracy(self):
         # return each category pixel accuracy(A more accurate way to call it precision)
